660c2eccbcaad7377ac675e5b8a27357fc963a04
김형찬 <koc0819@gmail.com>
2020-04-15 00:06:05 +0900

    day-1
    js 실습겸 타격대 근무일지  프로젝트

---
+++ prac.js
@@ -0,0 +1,20 @@
+let ap_num;
+
+function submitNum(){
+    ap_num=document.getElementById('ap_num');
+    console.log("ap_num : " + ap_num.value);
+}
+
+function submitData(){
+    let out_arr;
+    out_arr=document.getElementsByClassName('out_info');
+    
+    console.log(typeof(out_arr));
+    for(var i=10;i>=0;i--){
+        if(out_arr[i].value===""){
+            
+        }
+    }
+
+    
+}
